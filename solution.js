a5827bd05c726f3cf9c12ea3538349c68e899f18
ankonovalova <68342499+ankonovalova@users.noreply.github.com>
2022-10-18 14:01:12 +0300

    Add files via upload

---
+++ solution.js
@@ -0,0 +1,31 @@
+'Задание №1.1.';
+let message;
+let ProductName;
+message = ProductName;
+message + ' ';
+message + 'за';
+message + ' ';
+let ProductPrice
+message + ProductPrice;
+message + ' ';
+message + 'теперь в корзине!';
+console.log(message);
+'Конец решения задания №1.1.';
+
+'Задание №1.2.';
+let newValue;
+let oldValue;
+newValue = oldValue;
+console.log(newValue + 1);
+'Конец решения задания №1.2.';
+
+'Задание №1.3.';
+let newSum;
+let oldSum;
+let difference;
+newSum = (oldSum + difference);
+let newSumText;
+newSumText = newSum + '₽';
+newSumText = `${newSum} ₽`;
+console.log(newSumText);
+'Конец решения задания №1.3.';
